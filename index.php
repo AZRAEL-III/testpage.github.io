f61938585ea08509e3d26417163187fd05a79465
AZRAEL-III <hascoet.vincent.pro@gmail.com>
2024-01-04 20:03:28 +0100

    changed contents

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<html>
-    <head>
-        <title>My first PHP Website</title>
-    </head>
-    <body>
-        <?php
-                    echo "<p>Hello World!</p>";
-        ?>
-    </body>
-</html>
+<?php echo file_get_contents("html/header.html"); ?>
+<?php echo file_get_contents("html/body.html"); ?>
+<?php echo file_get_contents("html/footer.html"); ?>
+<p>Copyright &copy; Azrael Queenston <?php echo date("Y"); ?></p>
